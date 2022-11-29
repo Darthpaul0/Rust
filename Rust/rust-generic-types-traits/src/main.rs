2d9033da3f541eeb5e1ccafff5534f811493924b
Darthpaul0 <pablofr95@gmail.com>
2022-11-29 12:55:49 +0100

    Generic types and traits

--- Rust/rust-generic-types-traits/src/main.rs
+++ Rust/rust-generic-types-traits/src/main.rs
@@ -1,22 +1,70 @@
-struct Container<T> {
-    value: T,
+struct Groups<T> {
+    inner: Vec<T>,
 }
 
-impl<T> Container<T> {
-    pub fn new(value: T) -> Self {
-        Container { value }
+impl<T> Groups<T> {
+    fn new(inner: Vec<T>) -> Self {
+        Groups { inner }
+    }
+}
+
+impl<T: PartialEq> Iterator for Groups<T> {
+    type Item = Vec<T>;
+
+    // TODO: Write the rest of this implementation.
+    fn next(&mut self) -> Option<Self::Item> {
+        // if the inner vector is empty, we are done
+        if self.inner.is_empty() {
+            return None;
+        }
+
+        // lets check the span of equal items
+        let mut cursor = 1;
+        let first = &self.inner[0];
+        for element in &self.inner[1..] {
+            if element == first {
+                cursor += 1;
+            } else {
+                break;
+            }
+        }
+
+        // we use the `Vec::drain` to extract items up until the cursor
+        let items = self.inner.drain(0..cursor).collect();
+
+        // return the extracted items
+        Some(items)
     }
 }
 
 fn main() {
-    assert_eq!(Container::new(42).value, 42);
-    assert_eq!(Container::new(3.14).value, 3.14);
-    assert_eq!(Container::new("Foo").value, "Foo");
+    let data = vec![4, 1, 1, 2, 1, 3, 3, -2, -2, -2, 5, 5];
+    // groups:     |->|---->|->|->|--->|----------->|--->|
     assert_eq!(
-        Container::new(String::from("Bar")).value,
-        String::from("Bar")
+        Groups::new(data).into_iter().collect::<Vec<Vec<_>>>(),
+        vec![
+            vec![4],
+            vec![1, 1],
+            vec![2],
+            vec![1],
+            vec![3, 3],
+            vec![-2, -2, -2],
+            vec![5, 5],
+        ]
     );
-    assert_eq!(Container::new(true).value, true);
-    assert_eq!(Container::new(-12).value, -12);
-    assert_eq!(Container::new(Some("text")).value, Some("text"));
+
+    let data2 = vec![1, 2, 2, 1, 1, 2, 2, 3, 4, 4, 3];
+    // groups:      |->|---->|---->|----|->|----->|->|
+    assert_eq!(
+        Groups::new(data2).into_iter().collect::<Vec<Vec<_>>>(),
+        vec![
+            vec![1],
+            vec![2, 2],
+            vec![1, 1],
+            vec![2, 2],
+            vec![3],
+            vec![4, 4],
+            vec![3],
+        ]
+    )
 }
