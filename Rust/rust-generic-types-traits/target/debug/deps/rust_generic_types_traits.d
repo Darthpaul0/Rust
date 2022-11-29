0abbffd362353bc3614e7db75f02f6360f272db4
Darthpaul0 <pablofr95@gmail.com>
2022-11-29 12:53:35 +0100

    Generic types and traits

---
+++ Rust/rust-generic-types-traits/target/debug/deps/rust_generic_types_traits.d
@@ -0,0 +1,5 @@
+C:\Users\pferna12\Desktop\Rust\rust-generic-types-traits\target\debug\deps\rust_generic_types_traits.exe: src\main.rs
+
+C:\Users\pferna12\Desktop\Rust\rust-generic-types-traits\target\debug\deps\rust_generic_types_traits.d: src\main.rs
+
+src\main.rs:
---
+++ Rust/rust-generic-types-traits/target/debug/rust-generic-types-traits.d
@@ -0,0 +1,1 @@
+C:\Users\pferna12\Desktop\Rust\rust-generic-types-traits\target\debug\rust-generic-types-traits.exe: C:\Users\pferna12\Desktop\Rust\rust-generic-types-traits\src\main.rs
