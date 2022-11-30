f7a8c4be9731e62af50927eb5518756ca42629e5
Darthpaul0 <pablofr95@gmail.com>
2022-11-30 15:39:15 +0100

    Rust tests

---
+++ Rust/rust-tests/target/debug/deps/rust_tests.d
@@ -0,0 +1,5 @@
+C:\Users\pferna12\Desktop\Rust\rust-tests\target\debug\deps\rust_tests.exe: src\main.rs
+
+C:\Users\pferna12\Desktop\Rust\rust-tests\target\debug\deps\rust_tests.d: src\main.rs
+
+src\main.rs:
