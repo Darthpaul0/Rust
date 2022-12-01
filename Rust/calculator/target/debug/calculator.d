ecb6c9aaf301715521466e6db9213614225dee77
Darthpaul0 <pablofr95@gmail.com>
2022-12-01 16:51:33 +0100

    Calculator project

---
+++ Rust/calculator/target/debug/calculator.d
@@ -0,0 +1,1 @@
+C:\Users\pferna12\Desktop\Rust\calculator\target\debug\calculator.exe: C:\Users\pferna12\Desktop\Rust\calculator\src\main.rs
---
+++ Rust/calculator/target/debug/deps/calculator.d
@@ -0,0 +1,5 @@
+C:\Users\pferna12\Desktop\Rust\calculator\target\debug\deps\calculator.exe: src\main.rs
+
+C:\Users\pferna12\Desktop\Rust\calculator\target\debug\deps\calculator.d: src\main.rs
+
+src\main.rs:
