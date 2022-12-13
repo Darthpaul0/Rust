f7caaafdb52f3bfe03ddec226b5cc04619cd117c
Darthpaul0 <pablofr95@gmail.com>
2022-12-13 13:14:52 +0100

    Iced.rs project

---
+++ Projects/icedrs-project-example/target/debug/deps/icedrs_project_example-18f62df5390f037a.d
@@ -0,0 +1,6 @@
+c:\Users\pferna12\Desktop\Rust\Projects\icedrs-project-example\target\debug\deps\icedrs_project_example-18f62df5390f037a.rmeta: src\main.rs src\data.rs
+
+c:\Users\pferna12\Desktop\Rust\Projects\icedrs-project-example\target\debug\deps\icedrs_project_example-18f62df5390f037a.d: src\main.rs src\data.rs
+
+src\main.rs:
+src\data.rs:
---
+++ Projects/icedrs-project-example/target/debug/deps/unicode_xid-5c47a351cf3b9ad7.d
@@ -0,0 +1,8 @@
+c:\Users\pferna12\Desktop\Rust\Projects\icedrs-project-example\target\debug\deps\unicode_xid-5c47a351cf3b9ad7.rmeta: C:\Users\pferna12\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.4\src\lib.rs C:\Users\pferna12\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.4\src\tables.rs
+
+c:\Users\pferna12\Desktop\Rust\Projects\icedrs-project-example\target\debug\deps\libunicode_xid-5c47a351cf3b9ad7.rlib: C:\Users\pferna12\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.4\src\lib.rs C:\Users\pferna12\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.4\src\tables.rs
+
+c:\Users\pferna12\Desktop\Rust\Projects\icedrs-project-example\target\debug\deps\unicode_xid-5c47a351cf3b9ad7.d: C:\Users\pferna12\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.4\src\lib.rs C:\Users\pferna12\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.4\src\tables.rs
+
+C:\Users\pferna12\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.4\src\lib.rs:
+C:\Users\pferna12\.cargo\registry\src\github.com-1ecc6299db9ec823\unicode-xid-0.2.4\src\tables.rs:
