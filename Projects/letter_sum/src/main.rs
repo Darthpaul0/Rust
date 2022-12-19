36b251c9ed5b9ec22e01bf01ef962aefdd17b07a
Darthpaul0 <pablofr95@gmail.com>
2022-12-19 17:34:35 +0100

    Letter sum project: part 1

---
+++ Projects/letter_sum/src/main.rs
@@ -0,0 +1,36 @@
+use std::collections::HashMap;
+
+/**
+ * Assign every lowercase letter a value, from 1 for a to 26 for z. Given a string of lowercase letters,
+ * find the sum of the values of the letters in the string.
+ */
+
+fn main() {
+    // Create new HasMap
+    let mut charmap: HashMap<char, i32> = HashMap::new();
+
+    // fill HashMap with letter and its value
+    for (i, letter) in ('a'..='z').enumerate() {
+        charmap.insert(letter, i as i32 + 1);
+    }
+
+    assert_eq!(lettersum(String::from("abcd"), &charmap), 10);
+    assert_eq!(lettersum(String::from(""), &charmap), 0);
+    assert_eq!(lettersum(String::from("a"), &charmap), 1);
+    assert_eq!(lettersum(String::from("z"), &charmap), 26);
+    assert_eq!(lettersum(String::from("cab"), &charmap), 6);
+    assert_eq!(lettersum(String::from("excellent"), &charmap), 100);
+    assert_eq!(
+        lettersum(String::from("microspectrophotometries"), &charmap),
+        317
+    );
+}
+
+fn lettersum(word: String, charmap: &HashMap<char, i32>) -> i32 {
+    // convert word to iterator
+    word.chars()
+        // check if a letter is in the HashMap
+        .map(|c| charmap.get(&c).unwrap_or(&0).to_owned())
+        // add the result obtained
+        .sum()
+}
---
+++ Projects/letter_sum/target/debug/deps/letter_sum-ba9f52bdedccce5f.d
@@ -0,0 +1,5 @@
+c:\Users\pferna12\Desktop\Rust\Projects\letter_sum\target\debug\deps\letter_sum-ba9f52bdedccce5f.rmeta: src\main.rs
+
+c:\Users\pferna12\Desktop\Rust\Projects\letter_sum\target\debug\deps\letter_sum-ba9f52bdedccce5f.d: src\main.rs
+
+src\main.rs:
