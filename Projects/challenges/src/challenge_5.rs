beef717f83ce5955a6a0399ac430a041ab1299f7
Darthpaul0 <pablofr95@gmail.com>
2022-12-29 13:13:48 +0100

    Challenge 5 commented

--- Projects/challenges/src/challenge_5.rs
+++ Projects/challenges/src/challenge_5.rs
@@ -16,27 +16,35 @@ pub fn words_equals_separated(
 
     // define the pre-cached word list of words
     let precached_list = assign_value(charmap);
-    let mut index = 0;
 
-    // search in the precached list words separated by the specified length
-    // with the specified sum
+    // aux variables
+    let mut index = 0;
     let error = (&8 as &i32, &vec![String::from("error")]);
     let no_word = &String::from("nothing");
+
+    // get those words with the specified lettersum
     let words = precached_list.get_key_value(&sum).unwrap_or(error).1;
     let mut flag = true;
 
     while flag {
+        // auxiliary variable to store the word to compare
         let tmp_word = words.get(index).unwrap_or(no_word);
+
+        // compare aux variable with each word
         for word in words {
+            // we break in the first occurrence
             if word.len().abs_diff(tmp_word.len()) == separation {
+                // push both words to show later
                 separated_words.push(tmp_word.to_owned());
                 separated_words.push(word.to_owned());
                 break;
             }
+            // break loop
             flag = false;
         }
         index += 1;
     }
+    // provide some information to user
     if separated_words.len() == 0 {
         separated_words.push(String::from("No words found!"));
         separated_words
