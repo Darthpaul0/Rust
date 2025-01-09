f2487853c52c4e38401192ff353de5b43e5eaae9
Pablo <pablofr95@gmail.com>
2025-01-09 13:46:17 +0100

    starting advent of code series

---
+++ Advent of Code/aof_2015/target/debug/deps/aof_2015-7f5fa6251f5e6e8b.d
@@ -0,0 +1,5 @@
+C:\Users\Pablo\Desktop\Formación\Programación\Rust\Advent of Code\aof_2015\target\debug\deps\libaof_2015-7f5fa6251f5e6e8b.rmeta: src/main.rs
+
+C:\Users\Pablo\Desktop\Formación\Programación\Rust\Advent of Code\aof_2015\target\debug\deps\aof_2015-7f5fa6251f5e6e8b.d: src/main.rs
+
+src/main.rs:
---
+++ Advent of Code/aof_2015/target/debug/deps/aof_2015-f750e8ba30320f83.d
@@ -0,0 +1,5 @@
+C:\Users\Pablo\Desktop\Formación\Programación\Rust\Advent of Code\aof_2015\target\debug\deps\libaof_2015-f750e8ba30320f83.rmeta: src/main.rs
+
+C:\Users\Pablo\Desktop\Formación\Programación\Rust\Advent of Code\aof_2015\target\debug\deps\aof_2015-f750e8ba30320f83.d: src/main.rs
+
+src/main.rs:
