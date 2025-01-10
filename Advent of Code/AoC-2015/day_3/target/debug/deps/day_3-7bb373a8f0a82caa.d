7aef1a9264cdbbbce9a9d1327b6a295203ee6d43
Pablo <pablofr95@gmail.com>
2025-01-10 16:08:08 +0100

    starting day 3

---
+++ Advent of Code/AoC-2015/day_3/target/debug/deps/day_3-7bb373a8f0a82caa.d
@@ -0,0 +1,5 @@
+C:\Users\Pablo\Desktop\Formación\Programación\Rust\Advent of Code\AoC-2015\day_3\target\debug\deps\libday_3-7bb373a8f0a82caa.rmeta: src/main.rs
+
+C:\Users\Pablo\Desktop\Formación\Programación\Rust\Advent of Code\AoC-2015\day_3\target\debug\deps\day_3-7bb373a8f0a82caa.d: src/main.rs
+
+src/main.rs:
